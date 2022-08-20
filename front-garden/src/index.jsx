5b2b87e371f05e2ed6a22c1c8163b953248bcaf0
victoriom <Lumv&5332>
2022-08-20 08:31:23 -0400

    changed from class to hooks& functions

--- front-garden/src/index.jsx
+++ front-garden/src/index.jsx
@@ -1,147 +1,106 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import {CanvasControl} from './js/canvas_utils';
 import {default_plant_list} from './js/plant_image_lookup';
 import {Diary} from './react/diary_components'
 
+const ip_addr = "0.0.0.0:9110" 
 
-class GotPlant extends React.Component {
-  constructor(props) {
-      super(props)
+function GotPlant(props) {
 
-      this.state = {
-          mode:"place",
-          user: props.user,
-          all_plant_info: [],
-          length: props.length,
-          width: props.width,
-          diary:{
-            x: props.width + 40,
-            y: 200,
-            plant_on_location: [],
-          }
-          //db_connected: false
-      }
-      this.plant_options= default_plant_list
-      this.canvas_ref = React.createRef()
-      this.number_plants = 0
-      this.ip_addr = "0.0.0.0:9110" 
-  }
+  const [mode,setMode] = useState("place");
+  const [user,setUser] = useState(props.user);
+  const [allPlantInfo,setAllPlantInfo] = useState([]);
+  const [length,setLength] = useState(props.length);
+  const [width,setWidth] = useState(props.width);
+  const [diary,setDiary] = useState({
+    x: props.width + 40,
+    y: 200,
+    plant_on_location: [],
+  });
+  let numberPlants= 0;
+
+  const canvas_ref = React.createRef()
+  const [canvas,setCanvas] = useState({
+    ref: canvas_ref,
+    util: undefined
+  });
 
 
   // This function happen once the component is mounted the first time
-  componentDidMount() {
-      const canvas = this.canvas_ref.current
-      this.canvas_util = new CanvasControl(canvas)
-
-      fetch(`http://${this.ip_addr}/v0/health`)
-          .then(() => {
-              console.log("got a response from the database")
-            //   this.setState({
-            //       db_connected: true
-            //   })
-          })
-          .catch((error) => {
-              console.log("did not connect to db")
-          });
-  }
-
-  componentDidUpdate() {}
-
-  componentWillUnmount() {
-      //this will activate if the component is removed 
-  }
-
-  render() {
-    return(
-      <div>
-        <div >
-          <PlantDropdown plant_options={this.plant_options} />
-          <div>
-            <label >Length: </label>
-            <input name="length" value={this.state.length} type="number" 
-              onChange={this.size_adjustment("length")}/>
-            <label>Width: </label>
-            <input name="width" value={this.state.width} type="number" 
-              onChange={this.size_adjustment("width")}/>
-            <button onClick={this.clear_button()}>Clear Plot</button>
-          </div>
-          <div>
-            <label >This garden/plot belongs to ~ </label>
-            <input name="owner" value={this.state.user} 
-                onChange={this.set_state_onchange("user")}/> 
-            <button onClick={this.load()}>Load</button>
-            <div><button onClick={this.save_plot()}>Save</button></div>
-          </div>
-        </div>
-        <canvas ref={this.canvas_ref} style={{border:"3px dotted #000000"}}
-          width={this.state.width} height={this.state.length}
-          onClick={this.canvas_onclick_switch()} />
-        
-        {/* <PlantedList plant_list={this.state.all_plant_info}/> */}
-        <ModeDropdown current_mode={this.state.mode} onChange={this.set_state_onchange("mode")}/>
-        <Diary diaryInfo={this.state.diary} dateClick={this.date_added()} />
-      </div>
-    )
-  }
+  useEffect(()=>{
+    const canvas_current = canvas_ref.current
+    const canvas_util = new CanvasControl(canvas_current)
+    setCanvas({
+      ref: canvas_ref,
+      util: canvas_util
+    })
+  },[]) //notice the empty array here
 
   // -------- Event Functions & Util -------------// 
 
-  clear_button() {
+  const clear_button = () => {
     return ((event) => {
-        this.set_plants_empty()
+        set_plants_empty()
     })
   }
 
-  size_adjustment(side) {
+  const size_adjustment = (side)=> {
       return ((event) => {
-          this.setState({
-              [side]: event.target.value,
-          })
-          this.set_plants_empty() 
+        if (side === "length") {
+          setLength(event.target.value)
+        } else if (side === "width"){
+          setWidth(event.target.value)
+        } 
+        set_plants_empty() 
       })
   }
-
-  set_state_onchange(state) {
+   
+  const changed_mode = () => {
     return ((event) => {
-        this.setState({
-            [state]: event.target.value
-        })
+      setMode(event.target.value);
+      console.log(mode)
     })
   }
 
-  set_plants_empty() {
-      this.canvas_util.clear()
-      this.number_plants = 0
-      this.setState({
-          all_plant_info: []
-      })
+  const changed_user = () => {
+    return ((event) => {
+      setUser(event.target.value);
+    })
+  }
+
+  const set_plants_empty = () => {
+      canvas.util.clear()
+      numberPlants =0 
+      setAllPlantInfo([])
   }
 
 
-  canvas_onclick_switch(){
-    if (this.state.mode === "place"){
-        return this.added_plant()
-    } else if (this.state.mode === "select"){
-        return this.select_plant()
+  const canvas_onclick_switch = () =>{
+    if (mode === "place"){
+        return added_plant()
+    } else if (mode === "select"){
+        return select_plant()
     }
   } 
 
-  added_plant() {
+  const added_plant = () => {
       return ((event) => {
             const plant_selected = document.getElementById("plant_selection")
 
-            this.canvas_util.visual_draw(plant_selected, event.pageX, event.pageY)
+            canvas.util.visual_draw(plant_selected, event.pageX, event.pageY)
 
-            this.add_plant_info(plant_selected, event.pageX, event.pageY)
+            add_plant_info(plant_selected, event.pageX, event.pageY)
 
         })
   }
 
   
-  select_plant() {
+  const select_plant = () => {
     return ((event) => {
+
+      // console.log("selecting plants")
 
       const x = event.pageX
       const y = event.pageY
@@ -149,58 +108,50 @@ class GotPlant extends React.Component {
       const in_click_range = (plant) => {return plant["x"]+ 64 > x && 
             plant["x"]- 64 < x && plant["y"]+ 64 > y && plant["y"]- 64 < y}
 
-      const plants_onlocation = this.state.all_plant_info.filter(in_click_range)
-
-      this.setState({
-              diary:{
-                    x:x, 
-                    y:y,
-                    plant_on_location: plants_onlocation
-                  }
+      const plants_onlocation = allPlantInfo.filter(in_click_range)
+      setDiary({
+            x:x, 
+            y:y,
+            plant_on_location: plants_onlocation
           })
+      // console.log(diary)
       })  
-
   }
 
-  add_plant_info(plant_selected, x, y){
-    const plant_info_copy = this.state.all_plant_info.slice()
+  const add_plant_info = (plant_selected, x, y) =>{
+    const plant_info_copy = allPlantInfo.slice()
+    
 
     plant_info_copy.push({
         "name": plant_selected.value,
         "x": x,
         "y": y,
         "data": [],
-        "id":this.number_plants
+        "id":numberPlants
     })
 
-    this.number_plants++
-
-    this.setState({
-        all_plant_info: plant_info_copy
-    })
+    //console.log(plant_info_copy)
+    numberPlants++
+    setAllPlantInfo(plant_info_copy)
   }
 
   //================ Diary functions ============
 
-  date_added(){return (plant)=> (event)=>{
+  const date_added = () =>{ return (plant)=> (event)=>{
         const label = document.getElementById(`label_insert_${plant.id}`).value 
         const date  = document.getElementById(`date_insert_${plant.id}`).value 
         
-        const img = this.extract_img(plant)
+        const img = extract_img(plant)
 
-        const plant_info_copy = this.state.all_plant_info.slice()
+        const plant_info_copy = allPlantInfo.slice()
 
         plant_info_copy[plant["id"]]["data"].push({"label":label,"date":date,"img":img})
 
-        this.setState({
-          all_plant_info: plant_info_copy
-        })
-
-        //console.log(plant_info_copy)
+        setAllPlantInfo(plant_info_copy)
     }  
   } 
 
-  extract_img(plant){
+  const extract_img= (plant) =>{
     const img_insert = document.getElementById(`img_insert_${plant.id}`)
     const img_file  = img_insert.files[0]
 
@@ -214,26 +165,26 @@ class GotPlant extends React.Component {
   //=============== Saving & Loading ===============
   /**
    * returns a function for react that sends a return to save:
-   * @json {"plant_list": this.state.all_plant_info, "owner":this.state.user,
+   * @json {"plant_list": this.state.allPlantInfo, "owner":this.state.user,
    *          "width": this.state.width, "length":this.state.length}
    */
-  save_plot() {
+  const save_plot = () =>{
 
-    return (() => {
-        fetch(`http://${this.ip_addr}/v0/save_plot`, {
+    return ((event) => {
+        fetch(`http://${ip_addr}/v0/save_plot`, {
                 method: "POST",
                 headers: {
                     'Content-Type': 'application/json' //needs to match the body 
                 },
                 body: JSON.stringify({
-                    "plant_list": this.state.all_plant_info,
-                    "owner": this.state.user,
-                    "width": this.state.width,
-                    "length": this.state.length
+                    "plant_list": allPlantInfo,
+                    "owner": user,
+                    "width": width,
+                    "length": length
                 }),
             })
             .then(() => {
-                    alert(`Request to save under ${this.state.user} sent`)
+                    alert(`Request to save under ${user} sent`)
                     console.log("request to save sent")
                 },
                 (error) => {
@@ -245,13 +196,13 @@ class GotPlant extends React.Component {
 
 /**
  * returns a function for react that loads a db_entry & a sorted list for canvas:
- * @json {"plant_list": this.state.all_plant_info, "owner":this.state.user,
+ * @json {"plant_list": this.state.allPlantInfo, "owner":this.state.user,
  *          "width": this.state.width, "length":this.state.length}
  */
-load() {
+const load = () =>{
     return ((event) => {
-        this.set_plants_empty()
-        fetch(`http://${this.ip_addr}/v0/load?user=${this.state.user}`)
+        set_plants_empty()
+        fetch(`http://${ip_addr}/v0/load?user=${user}`)
             .then(response => {
                     return response.json()
                 },
@@ -262,17 +213,47 @@ load() {
                 if (response["plants"].length === 0){
                   alert("Load is empty")
                 }
-                this.canvas_util.visual_load(response["plants"])
-                this.setState({
-                    all_plant_info: response["plants"],
-                    length: response["grow_location"]["length"],
-                    width: response["grow_location"]["width"],
-                })
+                canvas.util.visual_load(response["plants"])
+                setAllPlantInfo(response["plants"])
+                setLength(response["grow_location"]["length"])
+                setWidth(response["grow_location"]["width"])
             });
     })
 }
 
-  
+
+return(
+      
+  <div>
+    <div >
+      <PlantDropdown plant_options={default_plant_list} />
+      <div>
+        <label >Length: </label>
+        <input name="length" value={length} type="number" 
+          onChange={size_adjustment("length")}/>
+        <label>Width: </label>
+        <input name="width" value={width} type="number" 
+          onChange={size_adjustment("width")}/>
+        <button onClick={clear_button()}>Clear Plot</button>
+      </div>
+      <div>
+        <label >This garden/plot belongs to ~ </label>
+        <input name="owner" value={user} 
+            onChange={changed_user()}/> 
+        <button onClick={load()}>Load</button>
+        <div><button onClick={save_plot()}>Save</button></div>
+      </div>
+    </div>
+  <canvas ref={canvas.ref} style={{border:"3px dotted #000000"}}
+    width={width} height={length}
+    onClick={canvas_onclick_switch()} />
+  {/* <PlantedList plant_list={allPlantInfo}/> */}
+  <ModeDropdown current_mode={mode} onChange={changed_mode()}/>
+  <Diary diaryInfo={diary} dateClick={date_added()} />
+</div>
+
+)
+
 }
 
 // ----------- State-less React Components ------------- // 
@@ -308,10 +289,23 @@ function ModeDropdown(props){
 }
 
 // ========================================
-
-  //ReactDOM.render(<GotPlant user={"Victorio_Natalie"} />, 
-  //                  document.getElementById("root"));
+  // ReactDOM.render(<NextUIProvider>
+  //                   <GotPlant user={"Vito"} length={550} width={1200} />
+  //                 </NextUIProvider>, 
+  //       document.getElementById("root"));
   const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<GotPlant user={"Victorio_Natalie"} length={550} width={1200}/>);
+  root.render(
+    <GotPlant user={"Vito"} length={550} width={1200}/>
+  );
   
 
+// fetch(`http://${ip_addr}/v0/health`)
+//     .then(() => {
+//         console.log("got a response from the database")
+//       //   this.setState({
+//       //       db_connected: true
+//       //   })
+//     })
+//     .catch((error) => {
+//         console.log("did not connect to db")
+//     });
