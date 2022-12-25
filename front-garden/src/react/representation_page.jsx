717e2fa541243f7491aa8f99b5a606067f4316b3
victoriom <Lumv&5332>
2022-12-25 22:46:08 +0000

    adding meta to work on android properl

--- front-garden/src/react/representation_page.jsx
+++ front-garden/src/react/representation_page.jsx
@@ -340,6 +340,7 @@ export function GotPage(props) {
     <>
       <Head>
         <title>{props.title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       </Head>
       <div>
         <CanvasComp mode={mode} itemList={currentRepInfo} onPress={canvasOnclickSwitch} 
